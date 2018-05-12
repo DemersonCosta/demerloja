d3edbd12389cd39b22155e5e1a0b2c3859e624e8
demersoncosta <demerson_pt@hotmail.com>
2018-05-12 17:21:25 -0300

    crud com create e update

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -59,7 +59,7 @@ class User extends Model{
 
     		header("Location: /admin/login");
 
-    		exit();
+    		exit;
     	}
     }
 
@@ -67,6 +67,43 @@ class User extends Model{
     {
     	$_SESSION[User::SESSION] = NULL;
 
+    }
+
+    public static function listAll()
+    {
+
+    	$sql = new Sql();
+
+    	return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+    	
+    }
+    public function save()
+    {
+        $sql = new Sql();
+
+        $results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+            ":desperson"=>$this->getdesperson(),
+            ":deslogin"=>$this->getdeslogin(),
+            ":despassword"=>$this->getdespassword(),
+            ":desemail"=>$this->getdesemail(),
+            ":nrphone"=>$this->getnrphone(),
+            ":inadmin"=>$this->getinadmin()
+        ));
+
+        $this->setData($results[0]);
+    }
+
+
+    public function get($iduser)
+    {
+
+        $sql =  new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser", array(
+            ":iduser"=>$iduser
+        ));
+
+        $this->setData($results[0]);
     }
 }
 
