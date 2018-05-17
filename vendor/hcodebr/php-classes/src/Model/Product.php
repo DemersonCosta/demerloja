84586b953063c61c87c4cd8f9e1eb32ab0799031
demersoncosta <demerson_pt@hotmail.com>
2018-05-17 15:10:50 -0300

    tela de product ok

--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -39,7 +39,7 @@ class Product extends Model {
       
     } 
 
-    public function get($idcategory)
+    public function get($idproduct)
     {
 
         $sql =  new Sql();
@@ -62,6 +62,70 @@ class Product extends Model {
 
         ]);
        
+    }
+
+    public function checkPhoto()
+    {
+        if (file_exists($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 
+            "res" . DIRECTORY_SEPARATOR .
+             "site" . DIRECTORY_SEPARATOR .
+             "img" . DIRECTORY_SEPARATOR . 
+             "Products" .DIRECTORY_SEPARATOR .
+             $this->getidproduct() . ".jpg"
+         )) {
+            $url = "/res/site/img/Products/" . $this->getidproduct() .".jpg";
+        }else{
+
+            $url = "/res/site/img/logo.png";
+        }
+
+        return $this->setdesphoto($url);
+    }
+
+    public function getValues()
+    {
+        $this->checkPhoto();
+
+        $values = parent::getValues();
+    
+        return $values;
+
+    }
+
+    public function setPhoto($file)
+    {
+
+        $extension = explode('.', $file['name']);
+        $extension = end($extension);
+
+        switch ($extension) {
+
+            case "jpg":
+            case "jpeg":
+                $image = imagecreatefromjpeg($file["tmp_name"]);
+                break;
+            case "gif":
+                $image = imagecreatefromgif($file["tmp_name"]);
+                break;
+            case "png":
+                $image = imagecreatefrompng($file["tmp_name"]);
+                break;        
+           
+        }
+
+        $dist = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 
+            "res" . DIRECTORY_SEPARATOR .
+             "site" . DIRECTORY_SEPARATOR .
+             "img" . DIRECTORY_SEPARATOR . 
+             "Products" .DIRECTORY_SEPARATOR .
+             $this->getidproduct() . ".jpg";
+
+        imagejpeg($image,$dist);
+
+        imagedestroy($image);
+
+        $this->checkPhoto();
+
     }
    
 }
